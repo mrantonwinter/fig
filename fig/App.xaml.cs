a25226401b3e053be6c578caf9203e63016d435f
mrantonwinter <sirantonwinter@gmail.com>
2015-02-08 13:05:40 +1100

    added xaml files

---
+++ fig/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using fig.Services;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace fig
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var c = new UnityContainer();
+
+            c.Resolve<MainWindow>().Show();
+        }
+
+    }
+}
