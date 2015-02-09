a68d26986fa1c909a0b96ad93d866957302e9e9a
mrantonwinter <sirantonwinter@gmail.com>
2015-02-10 06:26:04 +1100

    added betback host

---
+++ figUnitTests/Hosts/BetBackHostUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace figUnitTests
+{
+    [TestClass]
+    public class BetBackHostUnitTest : BaseUnitTest
+    {
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        //phone bet host local
+
+        [TestMethod]
+        [TestCategory("betback host local")]
+        public void BetBackHostLocal()
+        {
+            Assert.Fail();
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        //phone bet host dev
+
+        [TestMethod]
+        [TestCategory("BetBack host dev")]
+        public void BetBackHostDev()
+        {
+            Assert.Fail();
+        }
+    }
+}
