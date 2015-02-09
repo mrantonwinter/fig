628e7322eb2cf862ecacd750b30a93ca45044fcf
antonwinter <antonw@beteasy.com.au>
2015-02-10 06:49:54 +1100

    put in correct local wcf api

--- figUnitTests/WCFAPI/AdminWCFAPIUnitTest.cs
+++ figUnitTests/WCFAPI/AdminWCFAPIUnitTest.cs
@@ -24,23 +24,100 @@ namespace figUnitTests
 
         [TestMethod]
         [TestCategory("Admin WCF API local")]
+        public void BetInterceptLocal()
+        {
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "BetIntercept.API")));
+        }
+
+        [TestMethod]
+        [TestCategory("Admin WCF API local")]
         public void ClientAdminLocal()
         {
-            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "client")));
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "ClientAdmin.API")));
         }
 
         [TestMethod]
         [TestCategory("Admin WCF API local")]
-        public void EventAdminLocal()
+        public void MessagingLocal()
         {
-            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "event")));
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "Messaging.API")));
+        }
+      
+        [TestMethod]
+        [TestCategory("Admin WCF API local")]
+        public void DomainLocal()
+        {
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "Domain.API")));
+        }      
+      
+        [TestMethod]
+        [TestCategory("Admin WCF API local")]
+        public void SearchLocal()
+        {
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "Search.API")));
+        }
+      
+        [TestMethod]
+        [TestCategory("Admin WCF API local")]
+        public void SettlerLocal()
+        {
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "Settler.API")));
         }
 
         [TestMethod]
         [TestCategory("Admin WCF API local")]
-        public void RiskAdminLocal()
+        public void RiskManagementLocal()
         {
-            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "risk")));
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "RiskManagement.API")));
+        }
+
+        [TestMethod]
+        [TestCategory("Admin WCF API local")]
+        public void MarketingAdminLocal()
+        {
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "MarketingAdmin.API")));
+        }
+
+        [TestMethod]
+        [TestCategory("Admin WCF API local")]
+        public void ReportingLocal()
+        {
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "Reporting.API")));
+        }
+
+        [TestMethod]
+        [TestCategory("Admin WCF API local")]
+        public void FeedAdminLocal()
+        {
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "FeedAdmin.API")));
+        }
+
+        [TestMethod]
+        [TestCategory("Admin WCF API local")]
+        public void FinanceLocal()
+        {
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "Finance.API")));
+        }
+
+        [TestMethod]
+        [TestCategory("Admin WCF API local")]
+        public void CreditCardLocal()
+        {
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "CreditCard.API")));
+        }
+      
+        [TestMethod]
+        [TestCategory("Admin WCF API local")]
+        public void ClientEmailSmsLocal()
+        {
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "ClientEmailSms.API")));
+        }
+
+        [TestMethod]
+        [TestCategory("Admin WCF API local")]
+        public void AdminLocal()
+        {
+            AssertResult(_c.Resolve<WCFChecker>().Status(Setting(_local, "Admin.API")));
         }
 
         ///////////////////////////////////////////////////////////////////////////////////////////
